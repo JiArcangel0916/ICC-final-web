65861025d24b0c37200733b672b054ac618e7bec
JiArcangel0916 <arcangelalbrecht@gmail.com>
2025-06-09 00:23:40 +0800

    added forum feature

--- scripts/home.js
+++ scripts/home.js
@@ -2,10 +2,30 @@ const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         console.log(entry)
         if (entry.isIntersecting){
-            entry.taget.classList.add("show");
+            entry.target.classList.add("show");
         }
     })
 })
 
 const textElements = document.querySelectorAll(".text");
-hiddenElements.forEach((el) => observer.observe(el));
+textElements.forEach((el) => observer.observe(el));
+
+const pubKey = "TK3wxxKCpJUa8Vgjb";
+const serviceId = "service_ic0dr4j";
+const templateId = "template_beerc5n"
+
+emailjs.init({
+  publicKey: pubKey,
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("contact-form");
+  if (form) {
+    form.addEventListener("submit", function (e) {
+      e.preventDefault();
+      emailjs.sendForm(serviceId, templateId, this)
+        .then(() => alert('Message sent!'))
+        .catch(error => alert('Failed to send email ' + JSON.stringify(error)));
+    });
+  }
+});
